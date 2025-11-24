bd7748392a916dc63941d03a6e68c6d43a2a32b8
NASSEN Samuel <samuel.nassen@uhasselt.be>
2025-11-24 15:20:10 +0100

    jaren analyse

---
+++ 16_jaren_samen.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+folder = 'jaren'
+jaren_folders = [f for f in os.listdir(folder)]
+
+file_zelfde_adres = '7_analyse_clusters_zelfde_adres.xlsx'
+file_100m = '14_analyse_clusters_straal_100m.xlsx'
+
+data_zelfde_adres = []
+data_100m = []
+
+for jaar in jaren_folders:
+    file_path_zelfde_adres = os.path.join(folder, jaar, file_zelfde_adres)
+    df_zelfde_adres = pd.read_excel(file_path_zelfde_adres)
+    df_zelfde_adres['jaar'] = jaar
+    data_zelfde_adres.append(df_zelfde_adres)
+
+    file_path_100m = os.path.join(folder, jaar, file_100m)
+    df_100m = pd.read_excel(file_path_100m)
+    df_100m['jaar'] = jaar
+    data_100m.append(df_100m)
+
+df_alles_zelfde_adres = pd.concat(data_zelfde_adres, ignore_index=True)
+df_alles_zelfde_adres.to_excel('16a_analyse_jaren_zelfde_adres.xlsx', index=False)
+
+df_alles_100m = pd.concat(data_100m, ignore_index=True)
+df_alles_100m.to_excel('16b_analyse_jaren_100m.xlsx', index=False)
